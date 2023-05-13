202e7c3ab0e3ef2a4e40d6494ae83305cc05a6b7
Sebastian Vidrea <vidreasebastian@gmail.com>
2023-05-13 08:27:24 +0300

    Add a tappable button

--- SampelApp/src/pages/Home.tsx
+++ SampelApp/src/pages/Home.tsx
@@ -1,22 +1,28 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButton, IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
 import './Home.css';
 
 const Home: React.FC = () => {
+  const onButtonTapped = () => {
+    console.log('Button tapped')
+  }
+  
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Sample App</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">Sample App</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer />
+        <IonButton onClick={() => onButtonTapped()}>
+          Tap me
+        </IonButton>
       </IonContent>
     </IonPage>
   );
